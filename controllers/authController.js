a8104340a9354d28a5f0037d540c26f6fcc4117d
Đinh Bách Thông <thongdb.dev@gmail.com>
2024-10-09 23:39:55 +0700

    Send Activate Account Email

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,14 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import { sendActivateAccountEmail } from './mailController.js';
 
 export const register = async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
 
     const user = await User.findOne({ email: email });
-    if (user) return res.status(400).json({ error: "User already exists." });
+    if (user) return res.status(400).json({ error: 'User already exists.' });
 
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
@@ -20,6 +21,15 @@ export const register = async (req, res) => {
       active: 0,
     });
     const savedUser = await newUser.save();
+
+    const activationToken = jwt.sign(
+      { id: savedUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: '15d' },
+    );
+
+    await sendActivateAccountEmail(savedUser, activationToken);
+    
     res.status(201).json(savedUser);
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -31,12 +41,14 @@ export const login = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email: email });
-    if (!user) return res.status(400).json({ error: "User does not exist." });
-    
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ error: "Invalid credentials."});
+    if (!user) return res.status(400).json({ error: 'User does not exist.' });
 
-    if (!user.active) return res.status(400).json({ error: "User is not active."});
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ error: 'Invalid credentials.' });
+
+    if (!user.active)
+      return res.status(400).json({ error: 'User is not active.' });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
---
+++ controllers/mailController.js
@@ -0,0 +1,60 @@
+import nodemailer from 'nodemailer';
+import Mailgen from 'mailgen';
+
+export const sendMail = async (message) => {
+  const EMAIL = process.env.EMAIL_ADDRESS;
+  const PASSWORD = process.env.EMAIL_PASSWORD;
+
+  const config = {
+    host: 'smtp-mail.outlook.com',
+    secureConnection: false,
+    port: 587,
+    tls: {
+      ciphers: 'SSLv3',
+    },
+    auth: {
+      user: EMAIL,
+      pass: PASSWORD,
+    },
+  };
+  const transporter = nodemailer.createTransport(config);
+  return transporter.sendMail(message);
+};
+
+export const sendActivateAccountEmail = async (user, token) => {
+  const EMAIL = process.env.EMAIL_ADDRESS;
+
+  const MailGenerator = new Mailgen({
+    theme: 'default',
+    product: {
+      name: 'Skills Ranking',
+      link: 'https://skills-ranking-ui.vercel.app/',
+    },
+  });
+
+  const response = {
+    body: {
+      name: `${user.firstName} ${user.lastName}`,
+      intro:
+        "Welcome to Skills Ranking! We're very excited to have you on board.",
+      action: {
+        instructions: 'To get started with Skills Ranking, please click here:',
+        button: {
+          color: '#f02c56',
+          text: 'Confirm your account',
+          link: `https://skills-ranking-ui.vercel.app/activate?token=${token}`,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+  const mail = MailGenerator.generate(response);
+  const message = {
+    from: EMAIL,
+    to: user.email,
+    subject: 'Skill Ranking - Activate Account',
+    html: mail,
+  };
+  return sendMail(message);
+};
